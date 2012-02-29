3902959e02a21419f838aedc6cb67aa21b70079e
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2012-02-29 07:12:23 -0500

    updating readme, progress with init.py game engine, adding more test midi files, updating requirements.pip

--- init.py
+++ init.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python
 
 import midi
-import json
 import pprint
-
 import sys
 import pygame
 
-#def dictify(d):
-#    return dict((name, getattr(d, name)) for name in dir(d) if not name.startswith('__'))
+midi_file = 'mary.mid'
+scale_y = 20
+scale_x = 0.5
+
+'''
+midi_file = 'plus.mid'
+scale_y = 5
+scale_x = 0.105
+'''
+
+pp = pprint.PrettyPrinter(indent=1)
+
+window_width = 800
+window_height = 600
+
+# create the screen
+window = pygame.display.set_mode((window_width, window_height))
+
+# store global game data in game_map
+game_map = {
+    'ground': []
+}
 
 def is_note_on_event(d):
     return str(d)[:16] == 'midi.NoteOnEvent'
@@ -29,21 +47,22 @@ def note_event_dict(d):
     }
 
 def init():
-    pygame.init()
-    pp = pprint.PrettyPrinter(indent=1)
-
     midi_dict = {}
-    midi_data = midi.read_midifile("test.mid")
+    midi_data = midi.read_midifile(midi_file)
 
     # build up dict of note on and off events grouped by pitch
+    prev_tick = 0
     for d in midi_data[1]:
         if is_note_event(d):
             n = note_event_dict(d)
+            n['tick'] += prev_tick
             p = n['pitch']
-            if p in midi_dict:
-                midi_dict[p].append(n)
-            else:
+            if p not in midi_dict:
                 midi_dict[p] = []
+            midi_dict[p].append(n)
+            prev_tick = n['tick']
+
+    print len(midi_dict)
 
     # sort each pitch's list of note events in time
     for p, n in midi_dict.iteritems():
@@ -59,28 +78,69 @@ def init():
             prev = d
         midi_dict[p] = new_n
 
-    # find pitch boundaries
+    # find pitch boundaries and set basic parameters
     pitches = sorted(midi_dict.keys())
     lowest = pitches[0]
-    highest = pitches[-1:][0]
+    #highest = pitches[-1:][0]
 
     pp.pprint(midi_dict)
 
-    #create the screen
-    window = pygame.display.set_mode((640, 480))
+    # generate map
+    for p, n in midi_dict.iteritems():
+        left_x = velocity = close_block = False
+        every_other = True  # midi is dumb, so noteOn/noteOn needs to be treated as noteOn/noteOff
+        for d in n:
+            if close_block:
+                right_x = d['tick'] * scale_x
+                left_y = window_height - (((p - (lowest - 1)) * scale_y))
+                if every_other:
+                    game_map['ground'].append({
+                        'rect': (left_x, left_y, (right_x - left_x) - 20, scale_y),
+                        'note': {
+                            'velocity': velocity,
+                            'pitch': p
+                        }
+                    })
+                every_other = not every_other
+                if not d['on']:
+                    close_block = False
 
-    #draw a line - see http://www.pygame.org/docs/ref/draw.html for more
-    pygame.draw.line(window, (255, 255, 255), (0, 0), (30, 50))
+            left_x = d['tick'] * scale_x
+            velocity = d['velocity']
+            close_block = True
 
-    #draw it to the screen
-    pygame.display.flip()
+    setup_pygame()
+
+def setup_pygame():
+    game_time = 0 - window_width + 50
+    pygame.init()
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(midi_file)
+    pygame.mixer.music.play()
 
     #input handling (somewhat boilerplate code):
     while True:
-       for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              sys.exit(0)
-          else:
-              print event
+        game_time += 3.6
+        update(game_time)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+            else:
+                print event
+
+def update(time):
+    # blacken out screen
+    pygame.draw.rect(window, (0, 0, 0), (0, 0, window_width, window_height))
+
+    # draw game map
+    for g in game_map['ground']:
+        rect = list(g['rect'])
+        rect[0] -= round(time)
+        rect = tuple(rect)
+        pygame.draw.rect(window, (0, 100, (g['note']['pitch'] % 12) * 20), rect)
+
+    #draw it to the screen
+    pygame.display.flip()
 
 init()
